72bd5e0ee81618efb2825db5281bfd3a0d5fc2e3
ivan-the-terrible <ivanchwalik@hotmail.com>
2024-08-16 17:24:03 -0400

    feat: hit API endpoint

--- bloodsugar_cursor.ps1
+++ bloodsugar_cursor.ps1
@@ -1,3 +1,4 @@
+
 $SetSystemCursor_Signature = @"
 [DllImport("user32.dll")]public static extern bool SetSystemCursor(IntPtr hcur, int id);
 
@@ -23,3 +24,20 @@ public static void RestoreCursors(){
 "@
 
 $SystemParametersInfo = Add-Type -MemberDefinition $SystemParametersInfo_Signature -Name "Win32SystemParametersInfo" -Namespace Win32Functions -PassThru
+
+$scriptPath = $MyInvocation.MyCommand.Path
+$absolutePath = Convert-Path -Path $scriptPath
+$directory = [System.IO.Path]::GetDirectoryName($absolutePath)
+
+$jsonUrl = "http://192.168.0.127:1337/api/v1/entries.json"
+$response = Invoke-RestMethod -Uri $jsonUrl -Method Get
+
+$sugar_value = $response[0].sgv
+
+if ($sugar_value -gt 175) {
+    # set the color yellow
+} else if ($sugar_value -lt 70) {
+    # set the color red
+} else {
+    # set the color green
+}
